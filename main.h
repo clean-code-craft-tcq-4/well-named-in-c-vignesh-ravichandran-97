c4fad3be1ca7f5c4a411d493de0728d468dad31b
vignesh <vickycit997@gmail.com>
2022-06-14 02:04:48 +0530

    t4

--- main.h
+++ main.h
@@ -16,9 +16,9 @@ typedef struct {
     enum MajorColor majorColor;
     enum MinorColor minorColor;
 } ColorPair;
-void ColorPairToString(const ColorPair* colorPair, char* buffer);
-ColorPair GetColorFromPairNumber(int pairNumber) ;
-void testNumberToPair(int pairNumber,enum MajorColor expectedMajor,enum MinorColor expectedMinor);
-int GetPairNumberFromColor(const ColorPair* colorPair);
-void testPairToNumber(enum MajorColor major,enum MinorColor minor,int expectedPairNumber);
+extern void ColorPairToString(const ColorPair* colorPair, char* buffer);
+extern ColorPair GetColorFromPairNumber(int pairNumber) ;
+extern void testNumberToPair(int pairNumber,enum MajorColor expectedMajor,enum MinorColor expectedMinor);
+extern int GetPairNumberFromColor(const ColorPair* colorPair);
+extern void testPairToNumber(enum MajorColor major,enum MinorColor minor,int expectedPairNumber);
 #endif
